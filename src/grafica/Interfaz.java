3946b6f9c26f841fc8367bfdff5d46a95abfd621
Manu <Manuel Pandolfi>
2017-11-07 12:16:00 -0300

    Almacenamiento de Puntajes
    Se agrego la opcion de puntajes a la barra de menu, en el cual se
    mostraran los mejores 10 puntajes del juego, junto con la fecha en que
    se obtuvieron.

--- src/grafica/Interfaz.java
+++ src/grafica/Interfaz.java
@@ -6,6 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import juego.ente.Ente;
 
@@ -69,11 +74,14 @@ public class Interfaz extends JFrame{
 		JMenu menu = new JMenu("Menu");
 		JMenuItem acercaDe = new JMenuItem("Acerca De");
 		JMenuItem salir = new JMenuItem("Salir");
-
+		JMenuItem puntaje = new JMenuItem("Puntaje");
+		
 		setJMenuBar(menuBar);
 		menuBar.add(menu);
 		menu.add(acercaDe);
+		menu.add(puntaje);
 		menu.add(salir);
+		
 		
 		acercaDe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -89,6 +97,35 @@ public class Interfaz extends JFrame{
 				int opt = JOptionPane.showConfirmDialog(null, "¿Está seguro que desa salir del juego?", "Salir", JOptionPane.WARNING_MESSAGE);
 				if(opt == JOptionPane.OK_OPTION)
 					System.exit(0);
+			}
+		});
+		puntaje.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String texto = "PUNTAJES HISTORICOS: \n";
+				try {
+					BufferedReader w = new BufferedReader(new FileReader("puntaje.txt"));
+					ArrayList<String> rows = new ArrayList<String>();
+					String s;
+					while((s = w.readLine())!=null)
+					    	rows.add(s);
+					Collections.sort(rows);
+					int i = 0;
+					if(rows.size() == 0)
+						texto += "Aun no hay puntajes almacenados.";
+					while(i<rows.size() && i<10) {
+						String puntaje = rows.get(rows.size()-(i+1));
+						String pts = puntaje.substring(0, 2);
+						String fecha = puntaje.substring(2);
+						texto+=""+(i+1)+"- Pts: "+pts+"   |   Fecha: "+fecha+"\n";
+						i++;
+					}
+					w.close();
+				}catch(IOException ex) {
+					//Si no existe el archivo puntajes aun...
+					texto += "Aun no hay puntajes almacenados.";
+				}
+			
+				JOptionPane.showMessageDialog(null, texto, "Puntajes", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		
